f02b629d1a7bfb8fcaf1a6e3c1c2741f840bde5c
Aakash Rao <mrao.aakash@gmail.com>
2023-02-18 15:32:36 +0530

    Added files

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 python test.py \
     --gpu_id 0 \
-    --pretrain_path ./pretrain/resnet_10_23dataset.pth \
-    --img_list ./brats_2021/images_t1  \
-    --data_root ./brats_2021/t1.txt  \
+    --pretrain_path /home/chs.rintu/Documents/office/researchxaakash/MedicalNet/pretrain/resnet_10_23dataset.pth \
+    --img_list /home/chs.rintu/Documents/office/researchxaakash/MedicalNet/brats_2021/images_t1  \
+    --data_root /home/chs.rintu/Documents/office/researchxaakash/MedicalNet/brats_2021/t1.txt  \
