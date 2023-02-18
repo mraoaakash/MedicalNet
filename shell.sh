fadda6f58cd9a88329d1d67168ac8a279fe0242d
Aakash Rao <mrao.aakash@gmail.com>
2023-02-18 15:29:25 +0530

    Added files

--- shell.sh
+++ shell.sh
@@ -1,1 +1,5 @@
-python test.py --gpu_id 0 --pretrain_path pretrain/resnet_10_23dataset.pth --img_list brats_2021/images_t1  --data_root brats_2021/t1.txt  
+python test.py \
+    --gpu_id 0 \
+    --pretrain_path ./pretrain/resnet_10_23dataset.pth \
+    --img_list ./brats_2021/images_t1  \
+    --data_root ./brats_2021/t1.txt  \
